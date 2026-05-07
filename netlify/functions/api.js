920b2afb4e791875ca1ce245ac297bca4e19d7ff
dixitdj99 <dixitdj99@gmail.com>
2026-05-07 16:29:13 +0530

    fix: remove duplicate declarations in api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -32,11 +32,6 @@ const paymentRoutes = require('../../server/routes/paymentRoutes');
 const maintenanceRoutes = require('../../server/routes/maintenanceRoutes');
 const { requireAuth } = require('../../server/middleware/auth');
 const orgRoutes = require('../../server/routes/orgRoutes');
-const partyRoutes = require('../../server/routes/partyRoutes');
-const maintenanceRoutes = require('../../server/routes/maintenanceRoutes');
-const profileRoutes = require('../../server/routes/profileRoutes');
-const paymentRoutes = require('../../server/routes/paymentRoutes');
-const vehicleAdvanceRoutes = require('../../server/routes/vehicleAdvanceRoutes');
 
 const app = express();
 app.use(cors());
@@ -58,7 +53,6 @@ apiRouter.use('/backup', backupRoutes);
 apiRouter.use('/public', publicRoutes);
 apiRouter.use('/lr', requireAuth, lrRoutes); // Legacy JK Super route
 apiRouter.use('/labour', labourRoutes);
-apiRouter.use('/parties', requireAuth, partyRoutes);
 apiRouter.use('/stock', requireAuth, stockRoutes); // Legacy
 apiRouter.use('/org', requireAuth, orgRoutes);
 apiRouter.use('/parties', requireAuth, partyRoutes);
@@ -66,18 +60,14 @@ apiRouter.use('/maintenance', requireAuth, maintenanceRoutes);
 apiRouter.use('/profiles', requireAuth, profileRoutes);
 apiRouter.use('/payments', requireAuth, paymentRoutes);
 apiRouter.use('/vehicle-advances', requireAuth, vehicleAdvanceRoutes);
+apiRouter.use('/stock-transfers', requireAuth, stockTransferRoutes);
 
 // JKL Routes
 apiRouter.use('/jkl/lr', requireAuth, jklLrRoutes);
 apiRouter.use('/jkl/stock', requireAuth, jklStockRoutes);
 apiRouter.use('/jkl/cashbook', requireAuth, jklCashbookRoutes);
 apiRouter.use('/vehicles', requireAuth, vehicleRoutes);
-apiRouter.use('/vehicle-advances', requireAuth, vehicleAdvanceRoutes);
-apiRouter.use('/stock-transfers', requireAuth, stockTransferRoutes);
 apiRouter.use('/mileage', requireAuth, mileageRoutes);
-apiRouter.use('/profiles', requireAuth, profileRoutes);
-apiRouter.use('/payments', requireAuth, paymentRoutes);
-apiRouter.use('/maintenance', requireAuth, maintenanceRoutes);
 
 // Root route for base URL pings
 apiRouter.all('/', (req, res) => {
@@ -131,7 +121,7 @@ app.use((req, res) => {
 
 const handler = serverless(app);
 
-// Force rebuild - 2026-05-07 15:53
+// Force rebuild - 2026-05-07 16:30
 module.exports.handler = async (event, context) => {
     console.log('[Netlify] Incoming:', event.httpMethod, event.path);
     return handler(event, context);
