724f313c55d679745e14bd0e5f75952b676093e3
dixitdj99 <dixitdj99@gmail.com>
2026-05-07 16:06:31 +0530

    fix: register backend routes and add defensive frontend checks

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -26,6 +26,11 @@ const publicRoutes = require('../../server/routes/publicRoutes');
 const labourRoutes = require('../../server/routes/labourRoutes');
 const { requireAuth } = require('../../server/middleware/auth');
 const orgRoutes = require('../../server/routes/orgRoutes');
+const partyRoutes = require('../../server/routes/partyRoutes');
+const maintenanceRoutes = require('../../server/routes/maintenanceRoutes');
+const profileRoutes = require('../../server/routes/profileRoutes');
+const paymentRoutes = require('../../server/routes/paymentRoutes');
+const vehicleAdvanceRoutes = require('../../server/routes/vehicleAdvanceRoutes');
 
 const app = express();
 app.use(cors());
@@ -49,6 +54,11 @@ apiRouter.use('/lr', requireAuth, lrRoutes); // Legacy JK Super route
 apiRouter.use('/labour', labourRoutes);
 apiRouter.use('/stock', requireAuth, stockRoutes); // Legacy
 apiRouter.use('/org', requireAuth, orgRoutes);
+apiRouter.use('/parties', requireAuth, partyRoutes);
+apiRouter.use('/maintenance', requireAuth, maintenanceRoutes);
+apiRouter.use('/profiles', requireAuth, profileRoutes);
+apiRouter.use('/payments', requireAuth, paymentRoutes);
+apiRouter.use('/vehicle-advances', requireAuth, vehicleAdvanceRoutes);
 
 // JKL Routes
 apiRouter.use('/jkl/lr', requireAuth, jklLrRoutes);
